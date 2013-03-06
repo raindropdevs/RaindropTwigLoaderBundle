854c216a34d9036eeb5689e3fbdfec42a6bcc9ce
Matteo Caberlotto <mcaber@gmail.com>
2013-03-06 21:41:52 +0100

    added proxy methods for filesystem loader

--- Loader/ChainLoader.php
+++ Loader/ChainLoader.php
@@ -2,16 +2,47 @@
 
 namespace Raindrop\TwigLoaderBundle\Loader;
 
+use \Twig_Loader_Chain as BaseChainLoader;
+
 /**
  * This class exists to make TwigLoaderBundle compatible
- * with KnpMenuBundle. https://github.com/KnpLabs/KnpMenuBundle
+ * with KnpMenuBundle @ https://github.com/KnpLabs/KnpMenuBundle
+ * Bridges some methods to FileSystemLoader as most of bundles relies
+ * on its existence.
  */
-class ChainLoader extends \Twig_Loader_Chain {
+class ChainLoader extends BaseChainLoader {
+
+    public function addPaths($paths) {
+        foreach ($paths as $path) {
+            $this->addPath($path);
+        }
+    }
+
     public function addPath($path) {
-        // my code?
+        if (is_array($path)) {
+            $this->addPaths($path);
+            return;
+        }
+
         foreach ($this->loaders as $loader) {
             if ($loader instanceof \Twig_Loader_Filesystem) {
                 $loader->addPath($path);
+            }
+        }
+    }
+
+    public function setPaths($paths, $namespace) {
+        foreach ($this->loaders as $loader) {
+            if ($loader instanceof \Twig_Loader_Filesystem) {
+                $loader->setPaths($paths, $namespace);
+            }
+        }
+    }
+
+    public function prependPath($path, $namespace) {
+        foreach ($this->loaders as $loader) {
+            if ($loader instanceof \Twig_Loader_Filesystem) {
+                $loader->prependPath($path, $namespace);
             }
         }
     }
