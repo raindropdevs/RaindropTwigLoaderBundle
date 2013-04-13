2705aacaf85fdcb2128d0924ec012ea95f195642
Matteo Caberlotto <mcaber@gmail.com>
2013-04-13 12:48:33 +0200

    added syntax highlight

---
+++ Resources/public/js/templateAdmin.js
@@ -0,0 +1,33 @@
+var templateAdmin = (function () {
+    return {
+        init: function () {
+            this.initCodeMirror();
+        },
+
+        initCodeMirror: function () {
+            var mixedMode = {
+                name: "htmlmixed",
+                scriptTypes: [{matches: /\/x-handlebars-template|\/x-mustache/i,
+                               mode: null},
+                              {matches: /(text|application)\/(x-)?vb(a|script)/i,
+                               mode: "vbscript"}]
+            };
+
+            var el = $(".raindropTemplateTextarea");
+
+            var editor = CodeMirror.fromTextArea(el.get(0), {
+                lineNumbers: true,
+                matchBrackets: true,
+                mode: mixedMode,
+                theme: 'solarized light',
+                viewportMargin: Infinity
+            });
+
+            editor.setSize(el.width() - 200, 500)
+        }
+    };
+}());
+
+$(function () {
+    templateAdmin.init();
+})
