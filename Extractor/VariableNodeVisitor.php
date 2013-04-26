6947b4d316c6e2af93c2eae70bda854abde88e26
Matteo Caberlotto <mcaber@gmail.com>
2013-04-26 13:25:38 +0200

    created twig variables extractor helper

---
+++ Extractor/VariableNodeVisitor.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Raindrop\TwigLoaderBundle\Extractor;
+
+
+/**
+ * Find out which variables are being use into a twig template
+ *
+ * @author teito
+ */
+class VariableNodeVisitor implements \Twig_NodeVisitorInterface
+{
+
+    private $enabled = false;
+    private $messages = array();
+
+    public function enable()
+    {
+        $this->enabled = true;
+        $this->messages = array();
+    }
+
+    public function disable()
+    {
+        $this->enabled = false;
+        $this->messages = array();
+    }
+
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function enterNode(\Twig_NodeInterface $node, \Twig_Environment $env)
+    {
+        if (!$this->enabled) {
+            return $node;
+        }
+
+        if (
+            $node instanceof \Twig_Node_Expression_Name
+        ) {
+            // extract variables nodes
+            $this->messages [$node->getAttribute('name')]= true;
+        }
+
+        return $node;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function leaveNode(\Twig_NodeInterface $node, \Twig_Environment $env)
+    {
+        return $node;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPriority()
+    {
+        return 0;
+    }
+}
